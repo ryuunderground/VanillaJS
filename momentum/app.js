c2ea91cdb0346143221d5ce2891ebdf3e6a7e455
ryuunderground <jaeking00001@gmail.com>
2023-08-23 23:09:40 +0900

    Js variables

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,11 @@
-alert("hi");
+const a = 10;
+const b = 2;
+const myName = "ryu";
+const veryLongVariableName = "Long long time ago...";
+// Js 에서 variables 띄어쓰기 대신 다음 단어를 대문자로 씀//
+
+console.log(a + b);
+console.log(a / b);
+console.log(a - b);
+console.log(a * b);
+console.log("hello" + myName);
