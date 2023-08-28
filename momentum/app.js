2e31533653bb2767d50d23b7f3763a6b33e5fecc
ryuunderground <jaeking00001@gmail.com>
2023-08-28 20:44:55 +0900

    booleans

--- momentum/app.js
+++ momentum/app.js
@@ -1,17 +1,10 @@
-const a = 10;
-var b = 2;
-let myName = "ryu";
-let veryLongVariableName = "Long long time ago...";
-// Js 에서 variables 띄어쓰기 대신 다음 단어를 대문자로 씀 //
-// const 와 let 차이: 전자는 상수, 가변적이지 않음 //
-// var: const와 let의 조상, 어디서든 업데이트 가능. 단, 언어의 보호를 받지 못함 //
-// var은 잘 안 씀. 의미도 없고 위험해서 //
-console.log(a + b);
-console.log(a / b);
-console.log(a - b);
-console.log(a * b);
-console.log("hello" + myName);
+// boolean //
 
-myName = "underground";
-
-console.log("My name is " + myName);
+const amIFat = true;
+const really = false;
+const run = null;
+let sth;
+console.log(amIFat);
+console.log(really);
+console.log(run);
+console.log(sth);
