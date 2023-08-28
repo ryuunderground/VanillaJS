fb20454003631e8a285a76992a786278ef9358e7
ryuunderground <jaeking00001@gmail.com>
2023-08-28 18:42:35 +0900

    const, let

--- momentum/app.js
+++ momentum/app.js
@@ -1,11 +1,16 @@
 const a = 10;
-const b = 2;
-const myName = "ryu";
-const veryLongVariableName = "Long long time ago...";
-// Js 에서 variables 띄어쓰기 대신 다음 단어를 대문자로 씀//
-
+var b = 2;
+let myName = "ryu";
+let veryLongVariableName = "Long long time ago...";
+// Js 에서 variables 띄어쓰기 대신 다음 단어를 대문자로 씀 //
+// const 와 let 차이: 전자는 상수, 가변적이지 않음 //
+// var: const와 let의 조상, 어디서든 업데이트 가능. 단, 언어의 보호를 받지 못함 //
 console.log(a + b);
 console.log(a / b);
 console.log(a - b);
 console.log(a * b);
 console.log("hello" + myName);
+
+myName = "underground";
+
+console.log("My name is " + myName);
