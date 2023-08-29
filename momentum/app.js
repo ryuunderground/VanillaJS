8ee002f9a14e5db1c70fe83046d5a206f092ad2f
ryuunderground <jaeking00001@gmail.com>
2023-08-30 01:11:13 +0900

    return

--- momentum/app.js
+++ momentum/app.js
@@ -1,23 +1,8 @@
-const calaulator = {
-  add: function (a, b) {
-    console.log(a + b);
-  },
-  minus: function (a, b) {
-    console.log(a - b);
-  },
-  times: function (a, b) {
-    console.log(a * b);
-  },
-  divide: function (a, b) {
-    console.log(a / b);
-  },
-  power: function (a, b) {
-    console.log(a ** b);
-  },
-};
+const age = 96;
+function calculateKrAge(ageOfForeigner) {
+  return ageOfForeigner + 2;
+}
 
-calaulator.add(5, 10);
-calaulator.minus(5, 10);
-calaulator.times(5, 10);
-calaulator.divide(5, 10);
-calaulator.power(2, 5);
+const krAge = calculateKrAge(age);
+
+console.log(krAge);
