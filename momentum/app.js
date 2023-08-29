4fce746bbe0eebada79bd6909f0ea5edaaed2ac0
ryuunderground <jaeking00001@gmail.com>
2023-08-30 00:56:31 +0900

    calc

--- momentum/app.js
+++ momentum/app.js
@@ -1,93 +1,23 @@
-const player = {
-  name: "ryu",
-  age: 25,
-  smart: true,
-  sayHello: function (otherPersonsName) {
-    console.log("Hello! " + otherPersonsName);
+const calaulator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  times: function (a, b) {
+    console.log(a * b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  square: function (a, b) {
+    console.log(a ** b);
   },
 };
 
-console.log(player, console);
-// console 내역 확인하기 //
-/* console {debug: ƒ, error: ƒ, info: ƒ, log: ƒ, warn: ƒ, …}
-assert
-: 
-ƒ assert()
-clear
-: 
-ƒ clear()
-context
-: 
-ƒ context()
-count
-: 
-ƒ count()
-countReset
-: 
-ƒ countReset()
-createTask
-: 
-ƒ createTask()
-debug
-: 
-ƒ debug()
-dir
-: 
-ƒ dir()
-dirxml
-: 
-ƒ dirxml()
-error
-: 
-ƒ error()
-group
-: 
-ƒ group()
-groupCollapsed
-: 
-ƒ groupCollapsed()
-groupEnd
-: 
-ƒ groupEnd()
-info
-: 
-ƒ info()
-log
-: 
-ƒ log()
-memory
-: 
-MemoryInfo {totalJSHeapSize: 10000000, usedJSHeapSize: 10000000, jsHeapSizeLimit: 2190000000}
-profile
-: 
-ƒ profile()
-profileEnd
-: 
-ƒ profileEnd()
-table
-: 
-ƒ table()
-time
-: 
-ƒ time()
-timeEnd
-: 
-ƒ timeEnd()
-timeLog
-: 
-ƒ timeLog()
-timeStamp
-: 
-ƒ timeStamp()
-trace
-: 
-ƒ trace()
-warn
-: 
-ƒ warn()
-Symbol(Symbol.toStringTag)
-: 
-"console"
-[[Prototype]]
-: 
-Object */
+calaulator.add(5, 10);
+calaulator.minus(5, 10);
+calaulator.times(5, 10);
+calaulator.divide(5, 10);
+calaulator.square(5, 10);
