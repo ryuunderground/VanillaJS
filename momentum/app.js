0b211fb319e28977a0cf0b2c4f5e2c1948f30d09
ryuunderground <jaeking00001@gmail.com>
2023-08-29 21:37:02 +0900

    console contents

--- momentum/app.js
+++ momentum/app.js
@@ -1,6 +1,93 @@
-const a = ["text", "call", 1234];
+const player = {
+  name: "ryu",
+  age: 25,
+  smart: true,
+  sayHello: function (otherPersonsName) {
+    console.log("Hello! " + otherPersonsName);
+  },
+};
 
-console.log(a);
-a[1] = "movie";
-console.log(a);
-// array 내부 요소 교체 //
+console.log(player, console);
+// console 내역 확인하기 //
+/* console {debug: ƒ, error: ƒ, info: ƒ, log: ƒ, warn: ƒ, …}
+assert
+: 
+ƒ assert()
+clear
+: 
+ƒ clear()
+context
+: 
+ƒ context()
+count
+: 
+ƒ count()
+countReset
+: 
+ƒ countReset()
+createTask
+: 
+ƒ createTask()
+debug
+: 
+ƒ debug()
+dir
+: 
+ƒ dir()
+dirxml
+: 
+ƒ dirxml()
+error
+: 
+ƒ error()
+group
+: 
+ƒ group()
+groupCollapsed
+: 
+ƒ groupCollapsed()
+groupEnd
+: 
+ƒ groupEnd()
+info
+: 
+ƒ info()
+log
+: 
+ƒ log()
+memory
+: 
+MemoryInfo {totalJSHeapSize: 10000000, usedJSHeapSize: 10000000, jsHeapSizeLimit: 2190000000}
+profile
+: 
+ƒ profile()
+profileEnd
+: 
+ƒ profileEnd()
+table
+: 
+ƒ table()
+time
+: 
+ƒ time()
+timeEnd
+: 
+ƒ timeEnd()
+timeLog
+: 
+ƒ timeLog()
+timeStamp
+: 
+ƒ timeStamp()
+trace
+: 
+ƒ trace()
+warn
+: 
+ƒ warn()
+Symbol(Symbol.toStringTag)
+: 
+"console"
+[[Prototype]]
+: 
+Object */
