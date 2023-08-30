fcd97885e8c7b3d0e89d9c5bbfddee146bc66dda
ryuunderground <jaeking00001@gmail.com>
2023-08-30 18:18:16 +0900

    Search 4 elements

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,7 @@
-document.title = "Hello from JS";
+const title = document.querySelector(".hello h1");
+// 조건에 해당되는 1가지 요소만 가져옴 //
+const title2 = document.querySelectorAll(".hello h1");
+
+title.innerText = "Help meeeeeeeeeeee";
+console.log(title);
+console.log(title2);
