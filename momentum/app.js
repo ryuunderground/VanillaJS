d49948a360e982e4424498d85b830c6a61a6e270
ryuunderground <jaeking00001@gmail.com>
2023-08-30 17:12:23 +0900

    conditional-2 or, and

--- momentum/app.js
+++ momentum/app.js
@@ -1,7 +1,14 @@
 const age = prompt("How old r u?");
 
-if (isNaN(age)) {
-  console.log("Please write as a number");
-} else {
-  console.log("Thx");
+if (isNaN(age) || age < 0) {
+  console.log("Please write as a correct number");
+} else if (age < 18) {
+  console.log("Too young.....");
+} else if (age >= 18 && age <= 50) {
+  // && and || or //
+  console.log("U may drink");
+} else if (age > 50 && age < 80) {
+  console.log("U may but U'd better not drink......");
+} else if (age >= 80) {
+  console.log("Do whatever u want");
 }
