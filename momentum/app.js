6e3156cffd3d0a582b17f7696aef716e597048c0
ryuunderground <jaeking00001@gmail.com>
2023-08-31 16:03:48 +0900

    CSS-JS 3

--- momentum/app.js
+++ momentum/app.js
@@ -2,11 +2,12 @@ const h1 = document.querySelector("div h1");
 
 function handleTitleClick() {
   const clickedClass = "clicked";
-  if (h1.className === clickedClass) {
-    h1.className = "";
+  h1.classList.toggle(clickedClass);
+  /* if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
   } else {
-    h1.className = clickedClass;
-  }
+    h1.classList.add(clickedClass);
+  } */
 }
 
 h1.addEventListener("click", handleTitleClick);
