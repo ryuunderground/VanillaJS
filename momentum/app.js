b3c65097bd1d265f9010cfc8b95dc958fdda6ae2
ryuunderground <jaeking00001@gmail.com>
2023-08-31 10:39:29 +0900

    events

--- momentum/app.js
+++ momentum/app.js
@@ -1,7 +1,11 @@
 const title = document.querySelector(".hello h1");
 // 조건에 해당되는 1가지 요소만 가져옴 //
-const title2 = document.querySelectorAll(".hello h1");
+const title2 = document.querySelector(".hello:nth-child(2) h1");
 
-title.innerText = "Help meeeeeeeeeeee";
-console.log(title);
-console.log(title2);
+function handleTitleClick() {
+  console.log("title was clicked");
+  title.style.color = "red";
+}
+
+title.style.color = "blue";
+title2.addEventListener("click", handleTitleClick);
