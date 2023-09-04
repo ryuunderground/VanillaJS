acbf21801749ee8250f3e4cf8816aa1fc5da1b8d
ryuunderground <jaeking00001@gmail.com>
2023-09-05 01:56:15 +0900

    combining string

--- momentum/app.js
+++ momentum/app.js
@@ -1,18 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+// 일반적으로 string만 포함된 변수는 모두 대문자로 표시 //
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
+  loginForm.classList.add("hidden");
   console.log(username);
-}
-
-function handleLinkClick(event) {
-  event.preventDefault();
-  alert("clicked!");
-  console.dir(event);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
