e6ca5d07f943f788301475187aa53036ea9dcb1b
ryuunderground <jaeking00001@gmail.com>
2023-09-09 01:25:11 +0900

    username save, load

--- momentum/app.js
+++ momentum/app.js
@@ -1,17 +1,26 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-
+const USERNAME_KEY = "username";
 const HIDDEN_CLASSNAME = "hidden";
-// 일반적으로 string만 포함된 변수는 모두 대문자로 표시 //
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
-  loginForm.classList.add("hidden");
-  console.log(username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
