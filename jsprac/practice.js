308915618b2810bd540944bba2f105a863054c99
ryuunderground <jaeking00001@gmail.com>
2023-10-13 00:43:43 +0900

    prac4

--- jsprac/practice.js
+++ jsprac/practice.js
@@ -1,19 +1,32 @@
-const dcal = document.querySelector("h2#clock");
-const christmasEve = new Date("2023-12-24 00:00:00");
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34",
+];
 
-function xMasDDay() {
-  const today = new Date();
-  const dDay = christmasEve - today;
-  const perDay = 1000 * 60 * 60 * 24;
-  const perHour = 1000 * 60 * 60;
-  const perMinute = 1000 * 60;
-  const gapDate = Math.floor(dDay / perDay);
-  const dHour = dDay - perDay * gapDate;
-  const gapHour = Math.floor(dHour / perHour);
-  const dMinute = dHour - perHour * gapHour;
-  const gapMinute = Math.floor(dMinute / perMinute);
-  const gapSecond = Math.floor((dMinute - perMinute * gapMinute) / 1000);
-  dcal.innerText = `${gapDate}d ${gapHour}h ${gapMinute}m ${gapSecond}s`;
+const bg = document.querySelector("body");
+const button = document.querySelector("button");
+
+function bgColorChanger() {
+  const ranColorsOne = colors[Math.floor(Math.random() * colors.length)];
+  const ranColorsTwo = colors[Math.floor(Math.random() * colors.length)];
+  const dbColor = `linear-gradient(${ranColorsOne}, ${ranColorsTwo})`;
+  bg.style.background = dbColor;
 }
 
-setInterval(xMasDDay, 1000);
+button.addEventListener("click", bgColorChanger);
